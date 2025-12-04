6bd2fe9ebd9ebfcec07b77767fb0fd2bc68b08cf
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-12-04 18:56:35 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect } from 'react';
-import 'global.css';
+import 'globals.css';
 export default function HomePage() {
   useEffect(() => {
 // Inject the full portfolio page dynamically
