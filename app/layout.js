2421244b800cd1efd42fcf4e7780657caaa626b0
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-12-05 02:07:04 +0000

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 
@@ -8,9 +9,12 @@ description: "Cybersecurity student portfolio website",
 
 
 export default function RootLayout({ children }) {
-return (
-<html lang="en">
-<body>{children}</body>
-</html>
-);
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
